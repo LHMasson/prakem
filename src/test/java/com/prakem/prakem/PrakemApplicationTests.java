408907061ab19c899f34bd1f088180605a24057f
Lucas Masson <lucas.masson001@gmail.com>
2024-11-24 13:22:17 -0300

    Initial Structure

---
+++ src/test/java/com/prakem/prakem/PrakemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakem.prakem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
