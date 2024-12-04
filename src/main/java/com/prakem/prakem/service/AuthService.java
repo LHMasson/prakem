59933cbd6284d8aaa5831d191f985bf3a640c270
Lucas Masson <lucas.masson001@gmail.com>
2024-12-04 19:28:09 -0300

    Add validations to login method

--- src/main/java/com/prakem/prakem/service/AuthService.java
+++ src/main/java/com/prakem/prakem/service/AuthService.java
@@ -2,19 +2,33 @@ package com.prakem.prakem.service;
 
 import com.prakem.prakem.dto.LoginRequest;
 import com.prakem.prakem.dto.LoginResponse;
+import com.prakem.prakem.entity.User;
 import com.prakem.prakem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AuthService {
 
     UserRepository userRepository;
+    JwtService jwtService;
 
     @Autowired
-    public AuthService(UserRepository userRepository){
+    public AuthService(UserRepository userRepository, JwtService jwtService){
         this.userRepository = userRepository;
+        this.jwtService = jwtService;
     }
 
     public LoginResponse login(LoginRequest loginRequest){
-        return new LoginResponse();
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+        if (user == null) {
+           return new LoginResponse(); // configurar falso
+        }
+
+        if (!user.verifyPassword(loginRequest.getPassword())) {
+            return new LoginResponse(); // configurar falso
+        }
+
+        //gerar jwt
+
+        return new LoginResponse(); // configurar true
     }
 }
